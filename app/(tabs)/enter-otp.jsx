d401a8c5a3e99d13ad0b0b71cc12134c1a6ada00
Vickysonaiya <vickysonaiya0189@gmail.com>
2025-06-14 16:46:46 +0530

    minnor fixes

---
+++ app/(tabs)/enter-otp.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const enter_otp = () => {
+  return (
+    <View>
+      <Text>enter_otp</Text>
+    </View>
+  )
+}
+
+export default enter_otp
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,5 +2,9 @@ import { Stack } from "expo-router";
 import "../global.css"; // Import global styles
 
 export default function RootLayout() {
-  return <Stack />;
-}
+  return (
+  <Stack screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="index"/>
+    <Stack.Screen name="/home"/>
+  </Stack>
+)}
