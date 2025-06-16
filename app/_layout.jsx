ccbeca98462e5ef3347f4a9aa4272d4590d0cc39
Vickysonaiya <vickysonaiya0189@gmail.com>
2025-06-16 11:52:40 +0530

    changes done

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,8 +3,5 @@ import "../global.css"; // Import global styles
 
 export default function RootLayout() {
   return (
-  <Stack screenOptions={{ headerShown: false }}>
-    <Stack.Screen name="index"/>
-    <Stack.Screen name="/home"/>
-  </Stack>
+  <Stack />
 )}
